2a4e7e95d12e35e94960ea0308a088c14bc84145
Ashar Armoghan <81111014+AsharArmoghan@users.noreply.github.com>
2025-02-01 00:40:30 +0530

    Delete SalesDashboard directory
    old code

--- SalesDashboard/Backend/Controllers/DashboardController.js
+++
@@ -1,5 +0,0 @@
-const dashboard = async (req, res, next) => {
-	return null;
-};
-
-module.exports = dashboard;
--- SalesDashboard/Backend/Models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const orderSchema = new mongoose.Schema({
-	userId: { type: mongoose.Types.ObjectId, ref: "User" },
-	products: [{ type: mongoose.Types.ObjectId, ref: "Product" }, { quantity: Number }],
-	orderDate: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Order", orderSchema);
